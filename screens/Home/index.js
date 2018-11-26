6dd7b886bc72e0b43a8e47a1374cac2af16b7e7f
stephane <stephane@novaway.fr>
2018-11-26 16:04:17 +0100

    💄 Create default HomeScreen

---
+++ screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello world !</Text>
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
